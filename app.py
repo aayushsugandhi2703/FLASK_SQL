f26767d2e825155591a808295afaee1a14a27d61
Aayush suagndhi <aayush.sugandhi@gmail.com>
2024-05-26 23:26:57 +0530

    added the create method for flask and database

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template,request, redirect, url_for,jsonify
+from flask import Flask, render_template,request, redirect, url_for,jsonify,flash
 
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = '\xbd\xa4\xe9_4[!\xe5oW\xc9\xfc\xdfM+\xaf\x9b\x88my\xd6\xfe\xd9T'  # Replace with your own secret key
+
 
 # Database connection
 db = mysql.connector.connect(
@@ -19,7 +21,19 @@ def index():
     cursor.execute('SELECT * FROM tasks')
     tasks = cursor.fetchall()
     return render_template('index.html', tasks=tasks, task = "TASK WEBPAGE")
-    cursor.close()
+
+@app.route('/create', methods=['POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['task']
+        description = request.form['description']
+        complete = request.form.get('complete', False)
+
+        # Insert data into the database
+        cursor.execute('INSERT INTO tasks (title, description, complete) VALUES (%s, %s, %s)', (title, description, complete))
+        db.commit()
+        flash('Task added successfully')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
